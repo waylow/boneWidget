cce5e6c2b8334958f83e956b6281767d632f4908
Wayne Dixon <thewaylow@gmail.com>
2026-05-29 20:15:51 +1000

    Adjust geometry nodes (wireframe rendering) to work with 5.2 and be backwards compatible. Now scales the thickness via a math node.

--- functions/preview_functions.py
+++ functions/preview_functions.py
@@ -219,21 +219,15 @@ def create_wireframe_copy(obj, use_color, color, thickness):
         'GeometryNodeInstanceOnPoints')
     node_curve_to_mesh = node_group.nodes.new('GeometryNodeCurveToMesh')
     node_join_geometry = node_group.nodes.new('GeometryNodeJoinGeometry')
+    # Thickness scaling (so it isn't so sensitive)
+    node_scale_thickness = node_group.nodes.new("ShaderNodeMath")
+    node_scale_thickness.operation = 'MULTIPLY'
+    node_scale_thickness.inputs[1].default_value = 0.1
 
     # Set initial values (internal)
     node_uv_sphere.inputs["Segments"].default_value = 8
     node_uv_sphere.inputs["Rings"].default_value = 8
     node_curve_circle.inputs["Resolution"].default_value = 8
-
-    # Position nodes for better visualization (optional)
-    node_input.location = (-400, 0)
-    node_uv_sphere.location = (-150, 100)
-    node_mesh_to_curve.location = (-150, -50)
-    node_curve_circle.location = (-150, -150)
-    node_instance_on_points.location = (100, 250)
-    node_curve_to_mesh.location = (100, -100)
-    node_join_geometry.location = (350, 0)
-    node_output.location = (550, 0)
 
     # Connect nodes
     node_group.links.new(
@@ -241,9 +235,11 @@ def create_wireframe_copy(obj, use_color, color, thickness):
     node_group.links.new(
         node_input.outputs["Geometry"], node_mesh_to_curve.inputs["Mesh"])
     node_group.links.new(
-        node_input.outputs["Thickness"], node_uv_sphere.inputs["Radius"])
+        node_input.outputs["Thickness"], node_scale_thickness.inputs[0])
     node_group.links.new(
-        node_input.outputs["Thickness"], node_curve_circle.inputs["Radius"])
+        node_scale_thickness.outputs[0], node_uv_sphere.inputs["Radius"])
+    node_group.links.new(
+        node_scale_thickness.outputs[0], node_curve_circle.inputs["Radius"])
     node_group.links.new(
         node_uv_sphere.outputs["Mesh"], node_instance_on_points.inputs["Instance"])
     node_group.links.new(
@@ -258,7 +254,8 @@ def create_wireframe_copy(obj, use_color, color, thickness):
         node_join_geometry.outputs["Geometry"], node_output.inputs["Geometry"])
 
     # scale this so it isn't so sensitive
-    geo_mod["Socket_2"] = (thickness / 10)
+    if bpy.app.version < (5, 2, 0):
+        geo_mod["Socket_2"] = thickness
 
     return copy
 
